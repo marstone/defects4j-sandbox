4d1f892abc79d7a8171164d9cc71344933c793b8
marstone <marstone.net@gmail.com>
2016-02-20 16:10:25 +0800

    init

---
+++ src/main/java/net/marstone/App.java
@@ -0,0 +1,22 @@
+package net.marstone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App  {
+	public static void main( String[] args ) {
+		System.out.println( App.max(3, 5) );
+		System.out.println( "Hello World!" );
+	}
+
+	public static int max(int x, int y) {
+		if(x > y) {
+			return x;
+		} else {
+			return y;
+		}
+	}
+
+
+}
